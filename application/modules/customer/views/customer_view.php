2ab0207a3c582e705598ca03cba19c24a47f4174
Bakasa Joshua <joshua.bakasa@dataposit.co.ke>
2018-01-23 16:44:43 +0300

    Remove too long

--- application/modules/customer/views/customer_view.php
+++ application/modules/customer/views/customer_view.php
@@ -169,7 +169,7 @@
                         });
                         setTimeout(function(){
                             $("#message").hide();
-                        }, 6000);
+                        }, 2000);
                         
                     }
                 });
